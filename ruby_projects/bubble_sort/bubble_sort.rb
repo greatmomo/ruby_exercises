4ab149a90bb5cabf5cae3b877a49ff020d429728
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-12-30 12:31:01 +0100

    Object Oriented Programming
    Also played with rubocop a bit

--- ruby_projects/bubble_sort/bubble_sort.rb
+++ ruby_projects/bubble_sort/bubble_sort.rb
@@ -1,24 +1,24 @@
+# frozen_string_literal: true
+
 def bubble_sort(input_array)
-  #iterate across array and compare each value to the next, swap if right is larger
+  # iterate across array and compare each value to the next, swap if right is larger
   array = input_array
   loop do
     unsorted = 0
     array.each_with_index do |val, index|
-      unless index == array.length - 1
-        if val > array[index + 1]
-          temp = array[index + 1]
-          array[index] = temp
-          array[index + 1] = val
-          unsorted += 1
-        end
-      end
+      next if index == array.length - 1
+
+      next unless val > array[index + 1]
+
+      temp = array[index + 1]
+      array[index] = temp
+      array[index + 1] = val
+      unsorted += 1
     end
-    if unsorted == 0
-      break
-    end
+    break if unsorted.zero?
   end
   array
 end
 
-print bubble_sort([4,3,78,2,0,2])
+print bubble_sort([4, 3, 78, 2, 0, 2])
 puts
