0f766d9b12795c3cb2ec0249b489a6f7070aa057
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-01-10 18:32:32 +0100

    assignment time targeting

--- ruby_projects/event_manager/lib/event_manager.rb
+++ ruby_projects/event_manager/lib/event_manager.rb
@@ -2,6 +2,7 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 require 'pry-byebug'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -46,6 +47,14 @@ def clean_phone(phone_number)
   end
 end
 
+def extract_hour(reg_date)
+  DateTime.strptime(reg_date, "%m/%d/%Y %H:%M").hour
+end
+
+def extract_weekday(reg_date)
+  DateTime.strptime(reg_date, "%m/%d/%Y %H:%M").hour
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -57,16 +66,21 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+peak_hours = Hash.new(0)
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone = clean_phone(row[:homephone])
+  peak_hours[extract_hour(row[:regdate])] += 1
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
 end
+
+puts peak_hours
 
 # next: Assignment: Clean Phone Numbers
