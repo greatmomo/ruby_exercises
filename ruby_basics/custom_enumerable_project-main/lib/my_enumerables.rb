39cafb7ce1e6a8c69a9d64896c6904d8ca4a9578
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-01-17 19:20:04 +0100

    custom enumerables part 1

---
+++ ruby_basics/custom_enumerable_project-main/lib/my_enumerables.rb
@@ -0,0 +1,41 @@
+module Enumerable
+  # Your code goes here
+  def my_each_with_index
+  end
+  
+  def my_select
+  end
+
+  def my_all?
+  end
+
+  def my_any?
+  end
+
+  def my_none?
+  end
+
+  def my_count
+  end
+
+  def my_map
+  end
+  
+  def my_inject
+  end
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    for value in self do
+      yield value
+    end
+  end
+end
