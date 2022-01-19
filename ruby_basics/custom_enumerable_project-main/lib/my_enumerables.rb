b8ed8e6dc1e794c045a3efdd11355039cff1a090
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-01-19 18:56:07 +0100

    my_inject

--- ruby_basics/custom_enumerable_project-main/lib/my_enumerables.rb
+++ ruby_basics/custom_enumerable_project-main/lib/my_enumerables.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 module Enumerable
   # Your code goes here
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
     i = 0
-    for value in self do
+    each do |value|
       yield [value, i]
       i += 1
     end
   end
-  
+
   def my_select(&block)
     return to_enum(:my_select) unless block_given?
 
     arr = []
 
-    for value in self do
+    each do |value|
       arr << value if block.call(value)
     end
     arr
@@ -24,7 +26,7 @@ module Enumerable
   def my_all?(&block)
     return to_enum(:my_all?) unless block_given?
 
-    for value in self do
+    each do |value|
       return false unless block.call(value)
     end
     true
@@ -33,7 +35,7 @@ module Enumerable
   def my_any?(&block)
     return to_enum(:my_and?) unless block_given?
 
-    for value in self do
+    each do |value|
       return true if block.call(value)
     end
     false
@@ -42,20 +44,20 @@ module Enumerable
   def my_none?(&block)
     return to_enum(:my_none?) unless block_given?
 
-    for value in self do
+    each do |value|
       return false if block.call(value)
     end
     true
   end
 
-  def my_count(arg=nil, &block)
+  def my_count(arg = nil, &block)
     count = 0
     if block_given?
-      self.each { |value| count += 1 if block.call(value) }
-    elsif arg != nil
-      self.each { |value| count += 1 if value == arg }
+      each { |value| count += 1 if block.call(value) }
+    elsif !arg.nil?
+      each { |value| count += 1 if value == arg }
     else
-      self.each { |value| count += 1 }
+      each { |_value| count += 1 }
     end
     count
   end
@@ -65,13 +67,30 @@ module Enumerable
 
     arr = []
 
-    for value in self do
+    each do |value|
       arr << block.call(value)
     end
     arr
   end
-  
-  def my_inject
+
+  def my_inject(arg1 = nil, arg2 = nil, &block)
+    accumulator = nil
+    arr = instance_of?(Range) ? to_a : self
+    accumulator = arg1 if !arg1.nil? && arg2.nil? && block_given?
+    accumulator = arg1 if !arg1.nil? && !arg2.nil?
+    if accumulator.nil?
+      accumulator = first
+      (0..arr.size - 2).each do |i|
+        accumulator = block.call(accumulator, arr[i + 1]) if block_given?
+        accumulator = accumulator.send arg1, arr[i + 1] unless block_given?
+      end
+    else
+      (0..arr.size - 1).each do |i|
+        accumulator = block.call(accumulator, arr[i]) if block_given?
+        accumulator = accumulator.send arg2, arr[i] unless block_given?
+      end
+    end
+    accumulator
   end
 end
 
@@ -84,12 +103,16 @@ class Array
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    for value in self do
+    each do |value|
       yield(value, index)
     end
   end
 end
 
 numbers = [11, 22, 33, 4, 5]
-output = numbers.my_none? {|value| value > 10}
-puts "output = #{output}"
+# control = numbers.inject {|sum, n| sum + n}
+symbol = numbers.my_inject(:+)
+argsym = numbers.my_inject(10, :+)
+block = numbers.my_inject { |sum, n| sum + n }
+argblock = numbers.my_inject(10) { |sum, n| sum + n }
+puts "symbol = #{symbol}, argsym = #{argsym}, block = #{block}, argblock = #{argblock}"
