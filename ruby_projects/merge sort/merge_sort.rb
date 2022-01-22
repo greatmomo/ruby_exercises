2fe8531a4a7272fe6395bafe6dc950f4527f8bbd
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-01-22 16:48:58 +0100

    merge_sort.rb
    Also included beautification
    - Rubocop has no complaints here!

--- ruby_projects/merge sort/merge_sort.rb
+++ ruby_projects/merge sort/merge_sort.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
 def merge_sort(arr)
+  return arr if arr.length <= 1
+
+  arr_left = merge_sort(arr[0...arr.length / 2].dup)
+  arr_right = merge_sort(arr[arr.length / 2..arr.length].dup)
+
+  combine(arr_left, arr_right)
 end
+
+def combine(arr_left, arr_right)
+  ret_arr = []
+
+  while !arr_left.empty? && !arr_right.empty?
+    ret_arr.push(arr_left.first < arr_right.first ? arr_left.shift : arr_right.shift)
+  end
+  ret_arr += arr_left unless arr_left.empty?
+  ret_arr += arr_right unless arr_right.empty?
+  ret_arr
+end
+
+arr = [4, 3, 5, 1, 2, 7, 6, 2, 5, 12, 33, 1, 7, 2]
+print merge_sort(arr)
+puts
--- ruby_projects/merge sort/warmup.rb
+++ ruby_projects/merge sort/warmup.rb
@@ -1,21 +1,22 @@
-def fibs(n)
-  return 0 if n == 1
+# frozen_string_literal: true
+
+def fibs(val)
+  return 0 if val == 1
+
   left = 0
   right = 1
   tmp = 0
-  (2..n).each do |i|
+  (2..val).each do
     tmp = right
-    right = right + left
+    right += left
     left = tmp
   end
-  return right
+  right
 end
 
-
-def fibs_rec(n)
-  return n <= 1 ? n : fibs_rec(n-1) + fibs_rec(n-2)
+def fibs_rec(val)
+  val <= 1 ? val : fibs_rec(val - 1) + fibs_rec(val - 2)
 end
-
 
 # puts fibs(gets.chomp.to_i)
 puts fibs_rec(gets.chomp.to_i)
