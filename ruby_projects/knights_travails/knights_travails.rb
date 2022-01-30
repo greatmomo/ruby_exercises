546167683ee0561993909a6970b92c97298f2877
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-01-30 09:42:31 +0100

    build_board

--- ruby_projects/knights_travails/knights_travails.rb
+++ ruby_projects/knights_travails/knights_travails.rb
@@ -1,5 +1,44 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
+# try all 8 possible positions
+# if not already visited and inside board
+# add to queue with distance + 1 from parent state
+# finally return distance when popped from queue
+
+# Graph class for knight's travails
+class Board
+  attr_accessor :nodes
+
+  def initialize
+    @nodes = []
+    build_board
+  end
+
+  def add_node(value)
+    @nodes << Node.new(value)
+  end
+
+  def build_board
+    (0..7).each do |i|
+      (0..7).each do |j|
+        add_node([i,j])
+      end
+    end    
+  end
+
+  def build_moveset
+    # for each node, check if [+-1|2, +-1|2] is on the board
+    # if it is, add an edge to that node
+    #   how do I do this efficiently to actually link all the nodes properly and not just the values?
+  end
+end
+
+board = Board.new
+
+board.nodes.each { |node| puts "#{node.value}" }
+
 # knight_moves([0,0],[1,2]) == [[0,0],[1,2]]
 # knight_moves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
 # knight_moves([3,3],[0,0]) == [[3,3],[1,2],[0,0]]
@@ -10,30 +49,3 @@
 #     [4,5]
 #     [2,4]
 #     [4,3]
-
-# board is [0,0] to [7,7]
-# it shouldn't be possible to leave that board
-# create a tree with every possible result? It's a graph, not a tree!
-# don't allow repeated nodes
-
-# try all 8 possible positions
-# if not already visited and inside board
-# add to queue with distance + 1 from parent state
-# finally return distance when popped from queue
-
-# graph is like a tree, but with a neighbors array
-# so we can build a graph where each value [0,0] to [7,7] is a node
-# and each of those has all its acceptable neighbors defined
-
-# Graph class for knight's travails
-class Graph
-  attr_accessor :nodes
-
-  def initialize
-    @nodes = []
-  end
-
-  def add_node(value)
-    @nodes << Node.new(value)
-  end
-end
