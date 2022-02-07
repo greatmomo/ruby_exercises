4ee1df5cf8c1c1ebc003221742016ef03fead849
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-02-07 21:17:45 +0100

    subject update

--- ruby_basics/rspec_intro/spec/calculator_spec.rb
+++ ruby_basics/rspec_intro/spec/calculator_spec.rb
@@ -2,22 +2,21 @@
 require './lib/calculator'
 
 describe Calculator do
+  subject { Calculator.new }
+
   describe "#add" do
     it "returns the sum of two numbers" do
-      calculator = Calculator.new
-      expect(calculator.add(5, 2)).to eql(7)
+      expect(subject.add(5, 2)).to eql(7)
     end
 
     it "returns the sum of more than two numbers" do
-      calculator = Calculator.new
-      expect(calculator.add(2, 5, 7)).to eql(14)
+      expect(subject.add(2, 5, 7)).to eql(14)
     end
   end
 
   describe "#multiply" do
     it "returns the product of two numbers" do
-      calculator = Calculator.new
-      expect(calculator.multiply(5, 2)).to eql(10)
+      expect(subject.multiply(5, 2)).to eql(10)
     end
   end
 end
