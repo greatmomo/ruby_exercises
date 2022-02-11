f521688fc2853c6e09610909d58eb6d097afacc6
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-02-11 18:17:40 +0100

    Update 11c_dog_spec.rb

--- ruby_testing-main/spec/11c_dog_spec.rb
+++ ruby_testing-main/spec/11c_dog_spec.rb
@@ -35,17 +35,23 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
+  subject(:nimbus) { described_class.new('Nimbus', 'goldendoodle', 'white') }
 
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when a dog is asked to talk' do
+    it 'says WOOF!' do
+      expect(nimbus.talk).to eq('WOOF!')
+    end
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(nimbus).to_not be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(nimbus).to be_sleeping
   end
 end
