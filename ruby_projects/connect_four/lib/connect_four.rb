bb40766150db78e400b71a63eff812bd2eeb6582
greatmomo <66915435+greatmomo@users.noreply.github.com>
2022-03-02 19:05:32 +0100

    rubocop fixes

--- ruby_projects/connect_four/lib/connect_four.rb
+++ ruby_projects/connect_four/lib/connect_four.rb
@@ -1,25 +1,19 @@
 # frozen_string_literal: true
 
-require_relative 'player.rb'
+require_relative 'player'
 
 class ConnectFour
   def initialize(minimum = 0, maximum = 6, height = 6, symbol_p1 = '☭', symbol_p2 = '☪')
     @minimum = minimum
     @maximum = maximum
     @height = height
-    @board = [[],[],[],[],[],[],[]]
+    @board = [[], [], [], [], [], [], []]
     @players = [Player.new(symbol_p1), Player.new(symbol_p2)]
     @current_player = 1
-    @last_played = [0,0]
+    @last_played = [0, 0]
   end
 
-  def players
-    @players
-  end
-
-  def current_player
-    @current_player
-  end
+  attr_reader :players, :current_player
 
   def play_game
     introduction
@@ -33,12 +27,12 @@ class ConnectFour
   def end_game
     print_board
     if board_full?
-      puts "Game Over! The board is full!"
+      puts 'Game Over! The board is full!'
     elsif game_over?(@last_played[0], @last_played[1])
       if @board[@last_played[0]][@last_played[1]] == players[0].symbol
-        puts "Game Over! Player 1 Wins!"
+        puts 'Game Over! Player 1 Wins!'
       else
-        puts "Game Over! Player 2 Wins!"
+        puts 'Game Over! Player 2 Wins!'
       end
     end
   end
@@ -47,12 +41,12 @@ class ConnectFour
     print_board
     print_prompt
     input = player_input(@minimum, @maximum) - 1
-    unless @board[input].length >= @height
+    if @board[input].length >= @height
+      puts 'Input error! The selected column is full!'
+    else
       @board[input] << players[current_player - 1].symbol
       @last_played = [input, @board[input].length - 1]
       @current_player = toggle_player
-    else
-      puts "Input error! The selected column is full!"
     end
   end
 
@@ -82,7 +76,12 @@ class ConnectFour
   end
 
   def game_over?(col, row)
-    return true if scan_vertical(col, row) >= 4 || scan_horizontal(col, row) >= 4 || scan_up_right(col, row) >= 4 || scan_up_left(col, row) >= 4
+    return true if scan_vertical(col,
+                                 row) >= 4 || scan_horizontal(col,
+                                                              row) >= 4 || scan_up_right(col,
+                                                                                         row) >= 4 || scan_up_left(col,
+                                                                                                                   row) >= 4
+
     false
   end
 
@@ -91,7 +90,7 @@ class ConnectFour
     row -= 1 while row >= 0 && @board[col][row] == symbol
     row += 1
     count = 0
-    while row <= @maximum && @board[col][row] == symbol do
+    while row <= @maximum && @board[col][row] == symbol
       count += 1
       row += 1
     end
@@ -103,7 +102,7 @@ class ConnectFour
     col -= 1 while col >= 0 && @board[col][row] == symbol
     col += 1
     count = 0
-    while col <= @maximum && @board[col][row] == symbol do
+    while col <= @maximum && @board[col][row] == symbol
       count += 1
       col += 1
     end
@@ -112,16 +111,20 @@ class ConnectFour
 
   def scan_up_right(col, row)
     symbol = @board[col][row]
-    up_right_col= col
-    up_right_row= row
-    while up_right_col.between?(0,@maximum) && up_right_row.between?(0,@maximum) && @board[up_right_col][up_right_row] == symbol do
-      up_right_col += 1 
+    up_right_col = col
+    up_right_row = row
+    while up_right_col.between?(0,
+                                @maximum) && up_right_row.between?(0,
+                                                                   @maximum) && @board[up_right_col][up_right_row] == symbol
+      up_right_col += 1
       up_right_row += 1
     end
     up_right_col -= 1
     up_right_row -= 1
     count = 0
-    while up_right_col.between?(0,@maximum) && up_right_row.between?(0,@maximum) && @board[up_right_col][up_right_row] == symbol do
+    while up_right_col.between?(0,
+                                @maximum) && up_right_row.between?(0,
+                                                                   @maximum) && @board[up_right_col][up_right_row] == symbol
       count += 1
       up_right_col -= 1
       up_right_row -= 1
@@ -133,14 +136,18 @@ class ConnectFour
     symbol = @board[col][row]
     up_left_col = col
     up_left_row = row
-    while up_left_col.between?(0,@maximum) && up_left_row.between?(0,@maximum) && @board[up_left_col][up_left_row] == symbol do
-      up_left_col -= 1 
+    while up_left_col.between?(0,
+                               @maximum) && up_left_row.between?(0,
+                                                                 @maximum) && @board[up_left_col][up_left_row] == symbol
+      up_left_col -= 1
       up_left_row += 1
     end
     up_left_col += 1
     up_left_row -= 1
     count = 0
-    while up_left_col.between?(0,@maximum) && up_left_row.between?(0,@maximum) && @board[up_left_col][up_left_row] == symbol do
+    while up_left_col.between?(0,
+                               @maximum) && up_left_row.between?(0,
+                                                                 @maximum) && @board[up_left_col][up_left_row] == symbol
       count += 1
       up_left_col += 1
       up_left_row -= 1
@@ -151,9 +158,9 @@ class ConnectFour
   def introduction
     puts <<~HEREDOC
 
-    \e[0mWelcome to \e[32mConnect Four!\e[0m
+      \e[0mWelcome to \e[32mConnect Four!\e[0m
 
-      Take turns placing your symbol until somebody has \e[32mfour in a row\e[0m.
+        Take turns placing your symbol until somebody has \e[32mfour in a row\e[0m.
     HEREDOC
   end
 
@@ -162,18 +169,18 @@ class ConnectFour
     puts
     (0..@height).each do |row|
       (@minimum..@maximum).each do |column|
-        print "| #{@board[column][@height - row].nil? ? "O" : @board[column][@height - row]} "
+        print "| #{@board[column][@height - row].nil? ? 'O' : @board[column][@height - row]} "
       end
       puts "|\n"
     end
-    puts "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"
+    puts '¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯'
   end
 
   def print_prompt
     puts <<~HEREDOC
-      1   2   3   4   5   6   7
+        1   2   3   4   5   6   7
 
-    \e[32mPlayer #{@current_player}\e[0m, type the number of the row you would like to play in then press \e[32mENTER\e[0m.
+      \e[32mPlayer #{@current_player}\e[0m, type the number of the row you would like to play in then press \e[32mENTER\e[0m.
     HEREDOC
   end
 end
